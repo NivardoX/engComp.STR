b271c261e232e0ef754d16d79546a8b53a353505
Nivardo <nivardo00@gmail.com>
2021-09-13 10:52:22 -0300

    DEV - threading in C

--- lab1/main.c
+++ lab1/main.c
@@ -1,3 +1,9 @@
+/**
+ * Author:    Nivardo Albuquerque Leitão de Castro
+ * Created:   13.09.2021
+ * 
+ **/
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,12 +12,17 @@
 #define CYCLE_FRENQUENCY 5
 
 
+// --- Prototypes ---
+void *fill_char_array (void *arg);
+// ------------------
+
+// --- Globals --- 
 pthread_t threads[CYCLE_FRENQUENCY];
 int thread_args [CYCLE_FRENQUENCY];
-
 char output[OUTPUT_SIZE];
+// ---------------
 
-void *function (void *arg) {
+void *fill_char_array (void *arg) {
 
     int offset = *((int *)arg);
     char symbol = offset + 64;
@@ -29,7 +40,7 @@ int main (){
 
     for (int thread_number = 0; thread_number < CYCLE_FRENQUENCY; thread_number++){
             thread_args[thread_number] = thread_number+1;
-            pthread_create(&threads[thread_number], NULL, function, &thread_args[thread_number]);   
+            pthread_create(&threads[thread_number], NULL, fill_char_array, &thread_args[thread_number]);   
 
     }
 
